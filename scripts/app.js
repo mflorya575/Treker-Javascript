e85a201d31346a9fbcfe80875c31118a156dbb98
Michael Florya <mih2303@mail.ru>
2024-08-28 13:01:29 +0300

    fix bug with data

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,11 @@ const HABBIT_KEY = 'HABBIT_KEY';
 /* page */
 const page = {
 	menu: document.querySelector('.menu__list'),
-    header: {
-        h1: document.querySelector('.h1'),
-        progressPercent: document.querySelector('.progress__percent'),
-        progressCoverBar: document.querySelector('.progress__cover-bar'),
-    }
+	header: {
+		h1: document.querySelector('.h1'),
+		progressPercent: document.querySelector('.progress__percent'),
+		progressCoverBar: document.querySelector('.progress__cover-bar'),
+	}
 }
 
 /* utils */
@@ -34,7 +34,6 @@ function rerenderMenu(activeHabbit) {
 	for (const habbit of habbits) {
 		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
 		if (!existed) {
-            // создание
 			const element = document.createElement('button');
 			element.setAttribute('menu-habbit-id', habbit.id);
 			element.classList.add('menu__item');
@@ -54,22 +53,22 @@ function rerenderMenu(activeHabbit) {
 	}
 }
 
-function rerenderHead() {
-    if (!activeHabbit) {
+function rerenderHead(activeHabbit) {
+	if (!activeHabbit) {
 		return;
 	}
-    page.header.h1.innerText = activeHabbit.name;
-    const progress = activeHabbit.days.length / activeHabbit.target > 1
-        ? 100
-        : activeHabbit.days.length / activeHabbit.target * 100;
-    page.header.progressPercent.innerText = progress.toFixed(0) + ' %';
-    page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
+	page.header.h1.innerText = activeHabbit.name;
+	const progress = activeHabbit.days.length / activeHabbit.target > 1
+		? 100
+		: activeHabbit.days.length / activeHabbit.target * 100;
+	page.header.progressPercent.innerText = progress.toFixed(0) + '%';
+	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`);
 }
 
 function rerender(activeHabbitId) {
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
 	rerenderMenu(activeHabbit);
-    rerenderHead(activeHabbit);
+	rerenderHead(activeHabbit);
 }
 
 /* init */
